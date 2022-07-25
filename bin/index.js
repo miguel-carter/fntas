b8da6197968d86e7e6abf9ba9bfba72058017db4
Miguel Carter <nullcarter@gmail.com>
2022-07-25 13:14:11 -0500

    yargs removed

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
-const { initialize, Users } = require('./utils/storage.js');
+const { initialize } = require("./utils/storage.js");
 
 initialize();
-
-
-
-
-
-
